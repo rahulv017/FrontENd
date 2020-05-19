e6da8f56eb2c5b7fe18c2a5d107e4d76a58f67c2
Rahul <vyasrahul017@gmail.com>
2020-05-19 20:38:22 +0530

    Handling and Showing API Calls

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Products } from '../products';
+import { HomeServiceService } from '../home-service.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-homepage',
@@ -7,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service:HomeServiceService) { }
+   list_prod=ELE;
+   items:Observable<any>;
   ngOnInit() {
-    
+    this.items=this.service.getFeatureProducts();
   }
 
 }
+
+let ELE:Products[]=[
+    {"name":"Abhash Gaandu","desc":"Nalaayak","id":1,"price":2},
+    {"name":"Abhash Mc","desc":"Nalaayak","id":1,"price":3},
+    {"name":"Abhash Bc","desc":"Nalaayak","id":1,"price":4},
+    {"name":"Abhishek Gaandu","desc":"Nalaayak","id":1,"price":6},
+    {"name":"Abhishek Mc","desc":"Nalaayak","id":1,"price":8},
+    {"name":"Shreyansh Chutiya","desc":"Nalaayak","id":1,"price":10}
+]
