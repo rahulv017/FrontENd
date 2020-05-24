222f4d2e301c2264dbe78d47fdb52f23667b8620
Abhishek Acharya <acharya.n.abhi@gmail.com>
2020-05-24 14:58:21 +0530

    Updated home

--- src/app/home-service.service.ts
+++ src/app/home-service.service.ts
@@ -50,6 +50,6 @@ export class HomeServiceService {
 
   getCategoryProducts(data: number): Observable<Products[]> {
     let url = `http://localhost:4000/categories/${data}/products`;
-    return this.http.get<Products[]>(url, data);
+    return this.http.get<Products[]>(url);
   }
 }
